48f29d0a04dd3af350630cbb2efd44bca6fd6bc8
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-05 23:20:41 -0800

    Updated env vars

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -1,14 +1,14 @@
-import { StackContext, NextjsSite } from "sst/constructs";
+import { Config, StackContext, NextjsSite } from "sst/constructs";
 
 export function Default({ stack }: StackContext) {
+  const NEXTAUTH_URL = new Config.Secret(stack, "NEXTAUTH_URL");
+  const NEXTAUTH_SECRET = new Config.Secret(stack, "NEXTAUTH_SECRET");
+  const GITHUB_ID = new Config.Secret(stack, "GITHUB_ID");
+  const GITHUB_SECRET = new Config.Secret(stack, "GITHUB_SECRET");
+
   const site = new NextjsSite(stack, "site", {
-    environment: {
-      NEXTAUTH_URL: "",
-      NEXTAUTH_SECRET: "",
-      GITHUB_ID: "",
-      GITHUB_SECRET: "",
-    },
     path: "packages/web",
+    bind: [NEXTAUTH_URL, NEXTAUTH_SECRET, GITHUB_ID, GITHUB_SECRET],
   });
   stack.addOutputs({
     SiteUrl: site.url,
