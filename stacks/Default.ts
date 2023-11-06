fdef10cbcc44f4142c721e13f94174e9a11324d2
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-06 07:58:23 -0800

    Commented out bind

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -1,14 +1,15 @@
-import { Config, StackContext, NextjsSite } from "sst/constructs";
+import { Config } from "sst/node/config";
+import {  StackContext, NextjsSite } from "sst/constructs";
 
 export function Default({ stack }: StackContext) {
-  const NEXTAUTH_URL = new Config.Secret(stack, "NEXTAUTH_URL");
-  const NEXTAUTH_SECRET = new Config.Secret(stack, "NEXTAUTH_SECRET");
-  const GITHUB_ID = new Config.Secret(stack, "GITHUB_ID");
-  const GITHUB_SECRET = new Config.Secret(stack, "GITHUB_SECRET");
+  // const NEXTAUTH_URL = new Config.Secret(stack, "NEXTAUTH_URL");
+  // const NEXTAUTH_SECRET = new Config.Secret(stack, "NEXTAUTH_SECRET");
+  // const GITHUB_ID = new Config.Secret(stack, "GITHUB_ID");
+  // const GITHUB_SECRET = new Config.Secret(stack, "GITHUB_SECRET");
 
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
-    bind: [NEXTAUTH_URL, NEXTAUTH_SECRET, GITHUB_ID, GITHUB_SECRET],
+    // bind: [NEXTAUTH_URL, NEXTAUTH_SECRET, GITHUB_ID, GITHUB_SECRET],
   });
   stack.addOutputs({
     SiteUrl: site.url,
